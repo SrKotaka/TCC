02a923a5d83353247e924fbae32c2ac1c6d4f921
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2024-03-08 08:29:58 -0300

    Third Day

--- DistroForum/src/src/Account.tsx
+++ DistroForum/src/src/Account.tsx
@@ -1,10 +1,31 @@
 import './Account.css'
+import {FaUser, FaLock} from 'react-icons/fa';
 
 function Account() {
   return (
     <>
       <div className='containerAccount'>
-        <h1>Pagina De Login</h1>
+        <div className='wrapperAccount'>
+          <form action=''>
+            <h1>Login</h1>
+            <div className='input-boxAccount'>
+              <input type='text' placeholder='Username' required />
+              <FaUser className='iconAccount'/>
+            </div>
+            <div className='input-boxAccount'>
+              <input type='password' placeholder='Password' required />
+              <FaLock className='iconAccount'/>
+            </div>
+            <div className='remember-forgotAccount'>
+              <label><input type='checkbox'/>Remember me</label>
+              <a href='#'>Forgot password?</a>
+            </div>
+            <button type='submit'>Login</button>
+            <div className='register-linkAccount'>
+              <p>Don't have an account?<a href='#'>Register</a></p>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
