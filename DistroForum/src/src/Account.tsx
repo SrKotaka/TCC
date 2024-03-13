529a928b1da7086f8df47e571c29005e9d6a7d6b
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2024-03-13 10:07:55 -0300

    Fourth day

--- DistroForum/src/src/Account.tsx
+++ DistroForum/src/src/Account.tsx
@@ -5,27 +5,25 @@ function Account() {
   return (
     <>
       <div className='containerAccount'>
-        <div className='wrapperAccount'>
-          <form action=''>
-            <h1>Login</h1>
-            <div className='input-boxAccount'>
-              <input type='text' placeholder='Username' required />
-              <FaUser className='iconAccount'/>
-            </div>
-            <div className='input-boxAccount'>
-              <input type='password' placeholder='Password' required />
-              <FaLock className='iconAccount'/>
-            </div>
-            <div className='remember-forgotAccount'>
-              <label><input type='checkbox'/>Remember me</label>
-              <a href='#'>Forgot password?</a>
-            </div>
-            <button type='submit'>Login</button>
-            <div className='register-linkAccount'>
-              <p>Don't have an account?<a href='#'>Register</a></p>
-            </div>
-          </form>
-        </div>
+        <form action=''>
+          <h1>Login</h1>
+          <div className='input-boxAccount'>
+            <FaUser className='iconAccount'/>
+            <input type='text' placeholder='Username' required />
+          </div>
+          <div className='input-boxAccount'>
+            <FaLock className='iconAccount'/>
+            <input type='password' placeholder='Password' required />
+          </div>
+          <button type='submit'>Login</button>
+          <div className='remember-forgotAccount'>
+            <label><input type='checkbox'/> Remember me </label>
+            <a href='#'>Forgot password?</a>
+          </div>
+          <div className='register-linkAccount'>
+            <p>Don't have an account? <a href='#'>Register</a></p>
+          </div>
+        </form>
       </div>
     </>
   )
