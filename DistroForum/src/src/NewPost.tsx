c6a9eeae99049528353524ef91a35674a49a889b
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2024-05-13 11:32:30 -0300

    Post System Base

--- DistroForum/src/src/NewPost.tsx
+++ DistroForum/src/src/NewPost.tsx
@@ -1,6 +1,7 @@
 import './NewPost.css';
 import {Link} from 'react-router-dom'
 import { FaReply } from 'react-icons/fa';
+import UploadIcon from '../assets/UploadIcon.png'
 
 function NewPost() {
 
@@ -8,8 +9,13 @@ function NewPost() {
     <>
     <Link to='/'><FaReply className='iconNewPost' /></Link>
       <div className='containerNewPost'>
-
-        
+        <form>
+          <input type='text' placeholder='Tittle problem' />
+          <input type='text' placeholder='Write here your problem' />
+          <img src={UploadIcon} alt='img' />
+          <input type='file'></input>
+          <input type='button' value='Post' />
+        </form>
       </div>
     </>
   );
