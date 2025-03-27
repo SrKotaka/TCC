d4dbdb5041756b0456b44adce35ac24865b39e69
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2025-03-27 10:06:13 -0300

    arrumando o problema de puxar todos os municipios de uma vez

--- front-end/src/MapaBrasil.jsx
+++ front-end/src/MapaBrasil.jsx
@@ -1,22 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import 'leaflet/dist/leaflet.css';
-import municipios from './municipios.json'; // Seu arquivo JSON
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import municipios from './municipios.json';
 import { useState } from 'react';
 import axios from 'axios';
 
 const MapaBrasil = () => {
   const [temps, setTemps] = useState({});
 
-  // Função para buscar temperatura
   const fetchTemperature = async (lat, lon, municipioNome) => {
-    // Verifica se a temperatura já foi buscada para evitar chamadas desnecessárias
     if (temps[municipioNome] !== undefined) return;
 
     const apiKey = '9afc53f9544d4a02a2e141129251102';
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}&aqi=no`;
     try {
       const response = await axios.get(url);
-      const temp = response.data.current.temp_c; // Temperatura em Celsius
+      const temp = response.data.current.temp_c;
       setTemps((prevTemps) => ({
         ...prevTemps,
         [municipioNome]: temp,
@@ -36,27 +37,29 @@ const MapaBrasil = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {municipios.map((municipio) => (
-        <Marker
-          key={municipio.nome}
-          position={[municipio.lat, municipio.lon]}
-          eventHandlers={{
-            click: () => {
-              fetchTemperature(municipio.lat, municipio.lon, municipio.nome);
-            },
-          }}
-        >
-          <Popup>
-            <h3>{municipio.nome}</h3>
-            <p>
-              Temperatura:{' '}
-              {temps[municipio.nome] !== undefined
-                ? `${temps[municipio.nome]}°C`
-                : 'Clique para carregar'}
-            </p>
-          </Popup>
-        </Marker>
-      ))}
+      <MarkerClusterGroup>
+        {municipios.map((municipio) => (
+          <Marker
+            key={municipio.nome}
+            position={[municipio.lat, municipio.lon]}
+            eventHandlers={{
+              click: () => {
+                fetchTemperature(municipio.lat, municipio.lon, municipio.nome);
+              },
+            }}
+          >
+            <Popup>
+              <h3>{municipio.nome}</h3>
+              <p>
+                Temperatura:{' '}
+                {temps[municipio.nome] !== undefined
+                  ? `${temps[municipio.nome]}°C`
+                  : 'Clique para carregar'}
+              </p>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 };
